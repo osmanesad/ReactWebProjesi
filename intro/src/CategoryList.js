2ea16ae5969ad36e9ed4fdfd367a5a1478ac0c07
G-0324D <byesad@gmail.com>
2022-08-05 00:35:40 +0300

    Update - constructor
    constructor yerine direkt state ile işlem yapıldı.

--- intro/src/CategoryList.js
+++ intro/src/CategoryList.js
@@ -2,16 +2,13 @@ import React, { Component } from "react";
 import { ListGroup, ListGroupItem } from "reactstrap";
 
 export default class CategoryList extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      categories: [
-        { categoryId: 1, categoryName: "Kitaplar" },
-        { categoryId: 2, categoryName: "Dergiler" },
-        { categoryId: 3, categoryName: "Kahve" },
-      ],
-    };
-  }
+  state = {
+    categories: [
+      { categoryId: 1, categoryName: "Kitaplar" },
+      { categoryId: 2, categoryName: "Dergiler" },
+      { categoryId: 3, categoryName: "Kahve" },
+    ],
+  };
 
   render() {
     return (
