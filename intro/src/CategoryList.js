aba15bacce1e9b817dd5fbe303f62f97ff824d74
G-0324D <byesad@gmail.com>
2022-08-10 03:03:14 +0300

    Update - CategoryList
    Kategorileri json dosyasından çektik.

--- intro/src/CategoryList.js
+++ intro/src/CategoryList.js
@@ -3,12 +3,7 @@ import { ListGroup, ListGroupItem } from "reactstrap";
 
 export default class CategoryList extends Component {
   state = {
-    categories: [
-      { categoryId: 1, categoryName: "Kitaplar" },
-      { categoryId: 2, categoryName: "Dergiler" },
-      { categoryId: 3, categoryName: "Kahve" },
-    ],
-    
+    categories: []
   };
 
   /* App.js içine taşındı
@@ -16,15 +11,26 @@ export default class CategoryList extends Component {
     this.setState({ currentCategory: category.categoryName });
   };
 */
+
+componentDidMount(){
+  this.getCategories();
+}
+
+  getCategories = () => {
+    fetch("http://localhost:3000/categories")
+      .then((response) => response.json()) // =>> şuarada response.json sonuna () eklemedim diye hata aldım. saçmalık!
+      .then((data) => this.setState({ categories: data }));
+  };
+
   render() {
     return (
       <div>
         <h3>{this.props.info.title}</h3>
         <ListGroup>
-          {this.state.categories.map((category) => (
+          {this.state.categories.map((category) => ( // map bir array function
             <ListGroupItem
               onClick={() => this.props.changeCategory(category)}
-              key={category.categoryId}
+              key={category.id}
             >
               {category.categoryName}
             </ListGroupItem>
